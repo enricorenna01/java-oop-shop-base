66c5f209b2305ca414d4101e1f278d700a007d0a
enricorenna01 <erenna39@gmail.com>
2025-01-29 16:59:11 +0100

    feat: added characteristics

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,7 +1,12 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
-    public static void main(String[] args) {
-        
-    }
+    
+    //Caratteristiche del prodotto
+    int codice;
+    String nome;
+    String descrizione;
+    float prezzo;
+    float iva;
+
 }
