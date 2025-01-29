7b8fb3b179958038f7d6e601a79aaf1051efe4ea
enricorenna01 <erenna39@gmail.com>
2025-01-29 17:26:36 +0100

    feat: added methods

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,7 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
     
     //Caratteristiche del prodotto
@@ -9,4 +11,26 @@ public class Prodotto {
     float prezzo;
     float iva;
 
+
+    //Metodi
+    public Prodotto(String nome, String descrizione, float prezzo, float iva){
+        Random rand = new Random();
+        this.codice = rand.nextInt(777777);
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    public float prezzoBase(){
+        return this.prezzo;
+    }
+
+    public float prezzoIva(){
+        return this.prezzo + this.prezzo * this.iva;
+    }
+
+    public String nomeCompleto(){
+        return this.codice + "-" + this.nome;
+    }
 }
